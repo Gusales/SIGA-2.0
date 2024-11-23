f32abc5890f0767f04813aa48b0db079943e4b7a
Gusales <dev.gussales@gmail.com>
2024-11-23 20:50:01 -0300

    refactor(Home): Code Cleanup na home
    Neste commit, removi o arquivo layout.tsx que era somente utilizado na home page e passei os códigos para o arquivo Home.tsx

--- resources/js/Pages/Aluno/Home.tsx
+++ resources/js/Pages/Aluno/Home.tsx
@@ -2,20 +2,21 @@ import { AuthenticatedLayout } from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 import { CarrosselAvisos } from './Components/CarrosselAvisos';
-import { AlunoLayout } from './Layout';
+import PerfilAluno from './Components/PerfilAluno';
 
 export default function Home() {
     return (
         <>
           <Head title="Home" />
           <AuthenticatedLayout>
-            <AlunoLayout>
-              <section>
-                <h2>Mural de Avisos!</h2>
+              <main className="inset-0 sm:hidden">
+                <PerfilAluno />
+                <section>
+                  <h2>Mural de Avisos!</h2>
 
-                <CarrosselAvisos />
-              </section>
-            </AlunoLayout>
+                  <CarrosselAvisos />
+                </section>
+              </main>
           </AuthenticatedLayout>
         </>
     );
